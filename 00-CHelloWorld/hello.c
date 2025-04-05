ec12a4eb35f4b74c1a30821868f6249605b3b4bc
mayraegarcia <mayrgarcia@frba.utn.edu.ar>
2025-04-05 20:26:13 -0300

    Tp 0

---
+++ 00-CHelloWorld/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Para generar el archivo output.txt ejecute los siguientes comandos: 
+gcc hello.c -o hello.exe --> para compilar el programa y generar un ejecutable
+./hello.exe > output.txt  --> para redirecionar la salida del ejecutable al archivo output.txt
+*/ 
